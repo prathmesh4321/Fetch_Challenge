15463eac734c2bb1bf77e4fd608ed0c6a7695d46
Prathmesh <prathmeshsambrekar7@gmail.com>
2023-05-24 17:02:51 -0700

    Completed Receipt Processing Challenge

---
+++ Fetch/urls.py
@@ -0,0 +1,23 @@
+"""Fetch URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from receipts.views import process_receipts, get_points
+
+urlpatterns = [
+    path('receipts/process', process_receipts),
+    path('receipts/<id>/points', get_points),
+]
+
